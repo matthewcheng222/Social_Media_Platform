3a8ecbc002860ed9f5800f7a4d441d2fba9f4a43
Matthew Cheng <38536795+matthewcheng222@users.noreply.github.com>
2021-04-10 22:44:24 +0100

    Create DeletedPost.java

---
+++ src/socialmedia/DeletedPost.java
@@ -0,0 +1,21 @@
+package socialmedia;
+
+public class DeletedPost {
+    // Private instance variables
+    int id;
+    String message;
+
+    public DeletedPost(int id, String message) {
+        this.id = id;
+        this.message = message;
+    }
+
+    // Public getters and setters
+    public int getDeletedPostID() {
+        return id;
+    }
+
+    public String getDeletedPostMessage() {
+        return message;
+    }
+}
