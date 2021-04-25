ed03bee2d8b3f0d3832ecc2438a6fc798e1d43a2
Matthew Cheng <38536795+matthewcheng222@users.noreply.github.com>
2021-04-25 14:25:00 +0100

    Update CommentPost.java

--- src/socialmedia/CommentPost.java
+++ src/socialmedia/CommentPost.java
@@ -1,39 +1,78 @@
 package socialmedia;
 
 public class CommentPost extends SocialMedia {
-    // Private instance variables
-    String handle;
-    int id;
-    String message;
-    int commentPostID;
+    // private instance variables
 
+    private String handle;
+    private int id;
+    private String message;
+    private int commentPostID;
+
+    /**
+     * Constructor for initializing commentPosts. 
+     * 
+     * @param handle account's handle.
+     * @param id original post's ID.
+     * @param message comment post's message.
+     */
     public CommentPost(String handle, int id, String message) {
         this.handle = handle;
         this.id = id;
         this.message = message;
     }
 
-    // Public getters and setters
+    // public getters and setters
+    
+    /**
+     * The method sets the ID of a specific comment post.
+     * 
+     * @param commentPostID comment post's ID.
+     */
     public void setCommentPostID(int commentPostID) {
         this.commentPostID = commentPostID;
     }
 
+    /**
+     * The method sets the original post's ID of a specific comment post.
+     * 
+     * @param id original post's ID.
+     */
     public void setOriginalPostID(int id) {
         this.id = id;
     }
 
+    /**
+     * The method returns the original post's ID of a specific comment post.
+     * 
+     * @return comment post's original post ID.
+     */
     public int getOriginalPostID() {
         return id;
     }
 
+    /**
+     * The method returns the handle of a specific comment post.
+     * 
+     * @return comment post's handle.
+     */
     public String getCommentPostHandle() {
         return handle;
     }
 
+    /**
+     * The method returns the ID of a specific comment post.
+     * 
+     * @return comment post's ID.
+     */
     public int getCommentPostID() {
         return commentPostID;
     }
     
+    /**
+     * The method returns the message of a specific comment post.
+     * 
+     * @return comment post's message.
+     */
     public String getCommentPostMessage() {
         return message;
     }
