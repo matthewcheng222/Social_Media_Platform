8845366cf8837f8207e0c2e9db6a4fc816adf9a9
Matthew Cheng <38536795+matthewcheng222@users.noreply.github.com>
2021-04-25 14:30:49 +0100

    Update EndorsePost.java

--- src/socialmedia/EndorsePost.java
+++ src/socialmedia/EndorsePost.java
@@ -1,39 +1,85 @@
 package socialmedia;
 
 public class EndorsePost extends SocialMedia {
-    // Private instance variables
-    String handle;
-    int id;
-    int endorseID;
-    String reference;
+    // private instance variables
+    private String handle;
+    private int id;
+    private int endorseID;
+    private String reference;
 
+    /**
+     * Constructor for initialising endorsement posts.
+     * 
+     * @param handle endorsement post's handle.
+     * @param id ID of the original post.
+     */
     public EndorsePost(String handle, int id) {
         this.handle = handle;
         this.id = id;
     }
 
-    // Public getters and setters
+    // public getters and setters
+
+    /**
+     * The method sets the ID of the endorsement post.
+     * 
+     * @param endorseID endorsement post's ID.
+     */
     public void setEndorsePostID(int endorseID) {
         this.endorseID = endorseID;
     } 
 
+    /**
+     * The method sets the post reference of the endorsement post.
+     * 
+     * @param reference endorsement post's reference.
+     */
     public void setEndorsePostReference(String reference) {
         this.reference = reference;
     }
 
+    /**
+     * The method sets the original post ID of the endorsement post.
+     * 
+     * @param id original post ID of the endorsement post.
+     */
     public void setOriginalPostID(int id) {
         this.id = id;
     }
 
+    /**
+     * The method returns the original post ID of the endorsement post.
+     * 
+     * @return original post ID of the endorsement post.
+     */
     public int getOriginalPostID() {
         return id;
     }
     
+    /**
+     * The method returns the handle of the endorsement post.
+     * 
+     * @return endorsement post's handle.
+     */
     public String getEndorsePostHandle() {
         return handle;
     }
 
+    /**
+     * The method returns the ID of the endorsement post.
+     * 
+     * @return endorsement post's ID.
+     */
     public int getEndorsePostID() {
         return endorseID;
+    }
+
+    /**
+     * The method returns the post reference of the endorsement post.
+     * 
+     * @return endorsement post's reference.
+     */
+    public String getEndorsePostReference() {
+        return reference;
     }
 }
